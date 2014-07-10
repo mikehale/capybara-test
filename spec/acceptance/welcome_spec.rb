bfe0e106e3435d185b9b48eab3a7faad12d483b1
Michael Hale <mike@hales.ws>
2014-07-10 18:14:50 -0400

    cleanup

--- spec/acceptance/welcome_spec.rb
+++ spec/acceptance/welcome_spec.rb
@@ -2,9 +2,7 @@ require "spec_helper"
 
 describe"acceptance: view home page", type: :feature do
   it "views the home page", js: true do
-    p [self.class.example]
-    p [self.class.example.full_description]
     visit '/'
-    render_page('test')
+    render_page(self.class.example.full_description)
   end
 end
